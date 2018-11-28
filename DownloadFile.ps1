daacc9700faea80cacb67e513119d434351e0d92
himsh <himanitalesara23@gmail.com>
2018-11-28 14:33:21 +0530

    Create DownloadFile.ps1

---
+++ DownloadFile.ps1
@@ -0,0 +1,18 @@
+﻿
+#Parameter URL to download 
+[CmdletBinding()]
+ param (
+  [Parameter(Mandatory = $true, Position = 0)]
+  $URL = ""
+)
+
+$url = $URL
+$output = "$PSScriptRoot\DowloadwithPowershell.zip"
+$start_time = Get-Date
+
+$wc = New-Object System.Net.WebClient
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$wc.DownloadFile($url, $output)
+
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
