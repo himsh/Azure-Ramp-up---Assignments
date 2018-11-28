eb2e568aeda573c4d0dc272c53f6f5581561dc39
himsh <himanitalesara23@gmail.com>
2018-11-28 11:57:58 +0530

    Create FizzBuzzProblemWithParameters.ps1

---
+++ FizzBuzzProblemWithParameters.ps1
@@ -0,0 +1,53 @@
+﻿
+[CmdletBinding()]
+ param (
+  [Parameter(Mandatory = $false, Position = 0)]
+  $Min = 1,
+
+  [Parameter(Mandatory = $false, Position = 1)]
+  $Max = 100,
+
+  [Parameter(Mandatory = $false, Position = 2)]
+  $number = 5
+)
+
+$count = 0
+
+function printFizzBuzz()
+ {
+
+ if($Min -ge $Max)
+     {
+        Write-Output("Start number must be less than end number")
+     }
+
+      
+  
+    $Output = ""
+
+    for ($X = $Min; $X -le $Max; $X++) {
+          
+          
+          if ($X % 15 -eq 0) 
+             { $Output += " FizzBuzz " }
+
+          elseif ($X % 3 -eq 0) { $Output += " Fizz " }
+
+          elseif ($x % 5 -eq 0) {$Output += " Buzz "}
+      
+          else { $Output += " " + $X  }
+       
+          $count = $count + 1
+
+          if($count -eq $number)
+          {
+             $Output += "`n"
+             $count = 0
+          }
+
+     }
+
+     Write-Output $Output
+ }
+   
+printFizzBuzz  
